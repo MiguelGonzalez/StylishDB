7ea0a051b00724bc3d319b322026ead1c71b7ba0
MiguelGonzalez <miguelgonzalezgomez@gmail.com>
2013-07-29 20:59:46 +0200

    Eliminados imports sin utilizar..

--- src/es/miguelgonzalezgomez/dataBaseFun/bd/ManejadorConsulta.java
+++ src/es/miguelgonzalezgomez/dataBaseFun/bd/ManejadorConsulta.java
@@ -9,8 +9,6 @@ import java.sql.SQLException;
 import java.sql.Statement;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.logging.Level;
-import java.util.logging.Logger;
 
 /**
  *
