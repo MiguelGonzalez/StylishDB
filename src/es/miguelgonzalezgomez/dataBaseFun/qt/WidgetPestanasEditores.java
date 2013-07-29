e0c7352f8dbcf33cfe2fbe1df353bf3db3be7a1c
MiguelGonzalez <miguelgonzalezgomez@gmail.com>
2013-07-29 22:14:04 +0200

    Deshabilitamos el movernos entre pestañas con Control Tab y Control Shit Tab. Se gestionará manualmente en el editor.

--- src/es/miguelgonzalezgomez/dataBaseFun/qt/WidgetPestanasEditores.java
+++ src/es/miguelgonzalezgomez/dataBaseFun/qt/WidgetPestanasEditores.java
@@ -1,5 +1,7 @@
 package es.miguelgonzalezgomez.dataBaseFun.qt;
 
+import com.trolltech.qt.core.Qt;
+import com.trolltech.qt.gui.QKeyEvent;
 import com.trolltech.qt.gui.QTabBar;
 import com.trolltech.qt.gui.QTabWidget;
 import com.trolltech.qt.gui.QWidget;
@@ -23,5 +25,45 @@ public class WidgetPestanasEditores extends QTabWidget {
     
     public void setPestanasEditor(QTabBar tabBar) {
         setTabBar(tabBar);
+    }
+    
+    @Override
+    protected void keyPressEvent(QKeyEvent event) {
+         if(esCambiarSiguientePestana(event)) {
+            return;
+        }
+        
+        if(esCambiarAnteriorPestana(event)) {
+            return;
+        }
+        
+        super.keyPressEvent(event);
+    }
+    
+    private boolean esCambiarSiguientePestana(QKeyEvent e) {
+        Qt.KeyboardModifiers modifiers = e.modifiers();
+        if(
+                modifiers.isSet(Qt.KeyboardModifier.ControlModifier)) {
+            if(e.key() == Qt.Key.Key_Tab.value()) {
+                System.out.println("TRUE");
+                return true;
+            }
+        }
+        
+        return false;
+    }
+    
+    private boolean esCambiarAnteriorPestana(QKeyEvent e) {
+        Qt.KeyboardModifiers modifiers = e.modifiers();
+        
+        if(
+                modifiers.isSet(Qt.KeyboardModifier.ControlModifier) &&
+                modifiers.isSet(Qt.KeyboardModifier.ShiftModifier)) {
+            if(e.key() == Qt.Key.Key_Backtab.value()) {
+                return true;
+            }
+        }
+        
+        return false;
     }
 }
