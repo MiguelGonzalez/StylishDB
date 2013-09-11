eabbdddd19bdd96eae04331840644389564f9a23
MiguelGonzalez <miguelgonzalezgomez@gmail.com>
2013-09-11 10:59:22 +0200

    Mejora de los tests. Se comprueba que el obtener una consulta sea por el número 1.

---
+++ test/es/miguelgonzalezgomez/dataBaseFun/bd/TestAnalizadorTextoConsulta.java
@@ -0,0 +1,139 @@
+package es.miguelgonzalezgomez.dataBaseFun.bd;
+
+import es.miguelgonzalezgomez.dataBaseFun.bd.domain.TiposBasesDeDatos;
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author Miguel González
+ */
+public class TestAnalizadorTextoConsulta {
+    
+    private AnalizadorTextoConsulta analizadorTextoConsulta;
+    
+    public TestAnalizadorTextoConsulta() {
+    }
+    
+    @BeforeClass
+    public static void setUpClass() {
+    }
+    
+    @AfterClass
+    public static void tearDownClass() {
+    }
+    
+    @Before
+    public void setUp() {
+    }
+    
+    @After
+    public void tearDown() {
+    }
+    
+    @Test
+    public void consultaSimpleOracle() {
+        String consultaSQLAnalizar = "SELECT * FROM TABLA";
+        analizadorTextoConsulta = new AnalizadorTextoConsulta(
+                TiposBasesDeDatos.TIPO_BASE_DATOS.ORACLE,
+                consultaSQLAnalizar);
+        
+        assertEquals(
+                "Se esperaba obtener una consulta SQL",
+                1,
+                analizadorTextoConsulta.numConsultasExistentes()
+        );
+        
+        assertEquals(
+                "Se esperaba obtener la consulta SQL inicial",
+                consultaSQLAnalizar,
+                analizadorTextoConsulta.getConsulta(1)
+        );
+        
+        assertEquals(
+                "Se esperaba que la consulta fuera de tipo Ejecutar",
+                true,
+                analizadorTextoConsulta.isEjecutarQuery(1)
+        );
+    }
+    
+    @Test
+    public void consultaDobleOracle() {
+        String consultaSQLAnalizar = "SELECT * FROM TABLA;SELECT * FROM TABLA";
+        analizadorTextoConsulta = new AnalizadorTextoConsulta(
+                TiposBasesDeDatos.TIPO_BASE_DATOS.ORACLE,
+                consultaSQLAnalizar);
+        
+        assertEquals(
+                "Se esperaba obtener dos consultas SQL",
+                2,
+                analizadorTextoConsulta.numConsultasExistentes()
+        );
+        
+        assertEquals(
+                "Se esperaba obtener la primera consulta SQL inicial",
+                "SELECT * FROM TABLA",
+                analizadorTextoConsulta.getConsulta(1)
+        );
+        
+        assertEquals(
+                "Se esperaba obtener la segunda consulta SQL inicial",
+                "SELECT * FROM TABLA",
+                analizadorTextoConsulta.getConsulta(2)
+        );
+        
+        assertEquals(
+                "Se esperaba que la primera consulta fuera de tipo Ejecutar",
+                true,
+                analizadorTextoConsulta.isEjecutarQuery(1)
+        );
+        
+        assertEquals(
+                "Se esperaba que la segunda consulta fuera de tipo Ejecutar",
+                true,
+                analizadorTextoConsulta.isEjecutarQuery(2)
+        );
+    }
+    
+    @Test
+    public void consultaComplejaOracle() {
+        String consultaSQLAnalizar = "SELECT * FROM TABLA wheRe palabra like \'\";\';SELECT * FROM TABLA";
+        analizadorTextoConsulta = new AnalizadorTextoConsulta(
+                TiposBasesDeDatos.TIPO_BASE_DATOS.ORACLE,
+                consultaSQLAnalizar);
+        
+        assertEquals(
+                "Se esperaba obtener dos consultas SQL",
+                2,
+                analizadorTextoConsulta.numConsultasExistentes()
+        );
+        
+        assertEquals(
+                "Se esperaba obtener la primera consulta SQL inicial",
+                "SELECT * FROM TABLA wheRe palabra like \'\";\'",
+                analizadorTextoConsulta.getConsulta(1)
+        );
+        
+        assertEquals(
+                "Se esperaba obtener la segunda consulta SQL inicial",
+                "SELECT * FROM TABLA",
+                analizadorTextoConsulta.getConsulta(2)
+        );
+        
+        assertEquals(
+                "Se esperaba que la primera consulta fuera de tipo Ejecutar",
+                true,
+                analizadorTextoConsulta.isEjecutarQuery(1)
+        );
+        
+        assertEquals(
+                "Se esperaba que la segunda consulta fuera de tipo Ejecutar",
+                true,
+                analizadorTextoConsulta.isEjecutarQuery(2)
+        );
+    }
+}
