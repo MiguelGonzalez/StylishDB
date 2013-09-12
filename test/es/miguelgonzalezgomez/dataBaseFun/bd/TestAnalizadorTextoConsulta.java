ed5a84cd6b65059936ee8004b337fadd019a1ad8
MiguelGonzalez <miguelgonzalezgomez@gmail.com>
2013-09-12 10:05:09 +0200

    Añadidos nuevos Tests con Merge, Inserts y SubSelects

--- test/es/miguelgonzalezgomez/dataBaseFun/bd/TestAnalizadorTextoConsulta.java
+++ test/es/miguelgonzalezgomez/dataBaseFun/bd/TestAnalizadorTextoConsulta.java
@@ -312,4 +312,104 @@ public class TestAnalizadorTextoConsulta {
                 analizadorTextoConsulta.isEjecutarQuery(2)
         );
     }
+    
+    @Test
+    public void consultaInsertSubSelect() {
+        String insertSubSelectOracleSQL = "INSERT INTO A (ID, ID_B, T, TI) VALUES (("
+                + "SELECT MAX(ID) + 1 FROM A), 1, 'L', 'Lo');";
+        String segundaConsulta = "SELECT * FROM TABLA";
+        
+        analizadorTextoConsulta = new AnalizadorTextoConsulta(
+                TiposBasesDeDatos.TIPO_BASE_DATOS.ORACLE,
+                insertSubSelectOracleSQL + segundaConsulta);
+        
+        assertEquals(
+                "Se esperaban obtener dos consulta SQL",
+                2,
+                analizadorTextoConsulta.numConsultasExistentes()
+        );
+        
+        assertEquals(
+                "Se esperaba obtener la consulta SQL inicial",
+                insertSubSelectOracleSQL.substring(0, insertSubSelectOracleSQL.length() - 1),
+                analizadorTextoConsulta.getConsulta(1)
+        );
+        
+        assertEquals(
+                "Se esperaba que la consulta no fuera de tipo Ejecutar",
+                false,
+                analizadorTextoConsulta.isEjecutarQuery(1)
+        );
+        
+        assertEquals(
+                "Se esperaba obtener la consulta SQL inicial",
+                segundaConsulta,
+                analizadorTextoConsulta.getConsulta(2)
+        );
+        
+        assertEquals(
+                "Se esperaba que la consulta fuera de tipo Ejecutar",
+                true,
+                analizadorTextoConsulta.isEjecutarQuery(2)
+        );
+    }
+    
+    @Test
+    public void consultaInsertSubSelectMergeSQL() {
+        String insertSubSelectMergeOracleSQL = "INSERT INTO Production.UpdatedInventory SELECT ProductID, "
+                + "LocationID, NewQty, PreviousQty  FROM (    MERGE Production."
+                + "ProductInventory AS pi USING (SELECT ProductID, SUM(OrderQty)  "
+                + "FROM Sales.SalesOrderDetail AS sod JOIN Sales.SalesOrderHeader "
+                + "AS soh ON sod.SalesOrderID = soh.SalesOrderID AND soh.OrderDate "
+                + "BETWEEN '20030701' AND '20030731' GROUP BY ProductID) AS src ("
+                + "ProductID, OrderQty) ON pi.ProductID = src.ProductID WHEN MATCHED"
+                + " AND pi.Quantity - src.OrderQty >= 0  THEN UPDATE SET pi.Quantity "
+                + "= pi.Quantity - src.OrderQty WHEN MATCHED AND pi.Quantity - src."
+                + "OrderQty <= 0  THEN DELETE OUTPUT $action, Inserted.ProductID, "
+                + "Inserted.LocationID, Inserted.Quantity AS NewQty, Deleted.Quantity"
+                + " AS PreviousQty) AS Changes (Action, ProductID, LocationID,"
+                + " NewQty, PreviousQty) WHERE Action = 'UPDATE';";
+        String segundaConsulta = "MERGE dbo.Tabla1 AS Target USING (SELECT ID,Campo1,"
+                + "Campo2,Campo3 FROM dbo.Tabla2) AS Source ON (Target.ID = Source.ID)"
+                + " WHEN MATCHED THEN UPDATE SET Target.Campo1 = Source.Campo1,"
+                + " Target.Campo2 = Source.Campo2 WHEN NOT MATCHED BY TARGET THEN"
+                + " INSERT (ID,Campo1,Campo2,Campo3) VALUES (Source.ID,Source."
+                + "Campo1,Source.Campo2, Source.Campo3) WHEN NOT MATCHED BY "
+                + "SOURCE THEN DELETE;";
+        
+        analizadorTextoConsulta = new AnalizadorTextoConsulta(
+                TiposBasesDeDatos.TIPO_BASE_DATOS.ORACLE,
+                insertSubSelectMergeOracleSQL + segundaConsulta);
+        
+        assertEquals(
+                "Se esperaban obtener dos consulta SQL",
+                2,
+                analizadorTextoConsulta.numConsultasExistentes()
+        );
+        
+        assertEquals(
+                "Se esperaba obtener la consulta SQL inicial",
+                insertSubSelectMergeOracleSQL.substring(0, insertSubSelectMergeOracleSQL.length() - 1),
+                analizadorTextoConsulta.getConsulta(1)
+        );
+        
+        assertEquals(
+                "Se esperaba que la consulta no fuera de tipo Ejecutar",
+                false,
+                analizadorTextoConsulta.isEjecutarQuery(1)
+        );
+        
+        assertEquals(
+                "Se esperaba obtener la consulta SQL inicial",
+                segundaConsulta.substring(0, segundaConsulta.length() - 1),
+                analizadorTextoConsulta.getConsulta(2)
+        );
+        
+        assertEquals(
+                "Se esperaba que la consulta fuera no de tipo Ejecutar",
+                false,
+                analizadorTextoConsulta.isEjecutarQuery(2)
+        );
+        
+    }
 }
